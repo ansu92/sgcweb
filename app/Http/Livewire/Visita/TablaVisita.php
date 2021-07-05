1ce641905f44801a01e2ba9a5c65e665e7c89a0f
SgcWeb2 <teamsgcweb2.0@gmail.com>
2021-07-05 16:40:52 -0400

    Archivos creados del modulo visita
    componentes live wire

---
+++ app/Http/Livewire/Visita/TablaVisita.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Visita;
+
+use Livewire\Component;
+
+class TablaVisita extends Component
+{
+    public function render()
+    {
+        return view('livewire.visita.tabla-visita');
+    }
+}
